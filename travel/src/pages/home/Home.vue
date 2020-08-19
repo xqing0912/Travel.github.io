14053652d5848cbec476c18a0a9564afd2ef0046
xqing0912 <64568046+xqing0912@users.noreply.github.com>
2020-08-19 11:59:46 +0800

    項目代碼初始化

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -1,10 +1,8 @@
 <template>
 <!-- template裏面的所有内容都需要被包裹在一個div裏面 -->
 <div>
-  <div class="home">目前home</div>
-  <!-- 頁面跳轉，相當於a標簽，to相當於href -->
-  <router-link to="/list" class="home">列表頁面</router-link>
-  </div>
+  hello world
+</div>
 </template>
 
 <script>
@@ -14,8 +12,5 @@ export default {
 </script>
 
 <style>
-  .home{
-    font-size: 30px;
-    color: crimson;
-  }
+
 </style>
--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -1,22 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
-import List from '@/pages/list/List'
 
 Vue.use(Router)
 
 export default new Router({
 
   // router就是路由網址的設定
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }, {
-      path: '/list',
-      name: 'List',
-      component: List
-    }
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
   ]
 })
