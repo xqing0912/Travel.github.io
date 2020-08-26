7f1abfe438f7eeed027af2f5bdb1f01bac358825
xqing0912 <64568046+xqing0912@users.noreply.github.com>
2020-08-26 16:17:11 +0800

    添加icon.vue ＆icon-style.styl

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -11,7 +11,7 @@
 /* 引入HomeHeader的組件頁面 */
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
-import HomeIcons from './components/Icons'
+import HomeIcons from './components/Icon'
 export default {
   name: 'Home',
   /* 因爲是局部組件，所以要在components屬性中注冊HomeHeader這一組件 */
