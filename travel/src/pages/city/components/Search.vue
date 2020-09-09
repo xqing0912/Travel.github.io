8a37b85e396bf2014b019dfc9f089f73d29221f7
xqing0912 <64568046+xqing0912@users.noreply.github.com>
2020-09-09 12:25:51 +0800

    add search component

---
+++ travel/src/pages/city/components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="請輸入城市名稱" />
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/cityStyle/citySearch.styl'
+</style>
