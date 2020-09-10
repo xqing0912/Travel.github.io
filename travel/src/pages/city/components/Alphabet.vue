8c3103d05e48daeb83ebeac0fc49f8b597233256
xqing0912 <64568046+xqing0912@users.noreply.github.com>
2020-09-10 11:35:38 +0800

    add Alphabet component

---
+++ travel/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="alphabet-list">
+      <ul class="list">
+          <li class="item">A</li>
+          <li class="item">A</li>
+          <li class="item">A</li>
+          <li class="item">A</li>
+      </ul>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/cityStyle/cityAlphabet.styl'
+</style>
