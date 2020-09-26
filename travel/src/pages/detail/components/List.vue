a8bd777b905a9c489b6510fd24a018d3ffa41593
xqing0912 <64568046+xqing0912@users.noreply.github.com>
2020-09-26 19:00:57 +0800

    write detail-list code

---
+++ travel/src/pages/detail/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="item" v-for="(item, index) of list" :key="index">
+            <div class="item-title border-bottom">
+                <span class="item-title-icon">
+                </span>
+                {{item.title}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/detailStyle/list.styl'
+</style>
